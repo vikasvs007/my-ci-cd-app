5099f4bb405c62d48dc9aeb87a5a7e8fa54fbaa8
Vikas V S <vikasvs6363163@gmail.com>
2025-05-10 00:14:16 +0530

    Update Jenkinsfile with diagnostics for test and email issues

--- src/test/java/com/example/app/MyCiCdAppApplicationTests.java
+++ src/test/java/com/example/app/MyCiCdAppApplicationTests.java
@@ -1,9 +1,9 @@
 package com.example.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
